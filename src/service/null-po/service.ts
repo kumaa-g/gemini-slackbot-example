abcd6c4ecb397fe96c6de12cc57076718c907ec8
jupemara <jumpei.arashi@arashike.com>
2024-02-22 20:37:59 +0900

    null-po service + controller

---
+++ src/service/null-po/service.ts
@@ -0,0 +1,13 @@
+export class NullPoService {
+  execute(): string {
+    return `\`\`\`
+　　Λ＿Λ　　＼＼
+　 （　・∀・）　　　|　|　ｶﾞｯ
+　と　　　　）　 　 |　|
+　　 Ｙ　/ノ　　　 人
+　　　 /　）　 　 < 　>_Λ∩
+　 ＿/し'　／／. Ｖ｀Д´）/
+　（＿フ彡　　　　　 　　/
+\`\`\``;
+  }
+}
