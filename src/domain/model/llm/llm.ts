64cb72970656b3dd77b44203758af1849e2edb99
jupemara <jumpei.arashi@arashike.com>
2024-02-22 22:27:58 +0900

    declare multi modal input

--- src/domain/model/llm/llm.ts
+++ src/domain/model/llm/llm.ts
@@ -1,6 +1,6 @@
-import { MessageInput } from '../message/input';
+import { TextInput } from '../message/input';
 import { MessageOutput } from '../message/output';
 
 export interface ILLM {
-  generate(input: MessageInput): Promise<MessageOutput>;
+  generate(input: TextInput): Promise<MessageOutput>;
 }
--- src/service/llm/service.ts
+++ src/service/llm/service.ts
@@ -1,11 +1,11 @@
 import { ILLM } from '~/domain/model/llm/llm';
-import { MessageInput } from '~/domain/model/message/input';
+import { TextInput } from '~/domain/model/message/input';
 
 export class LLMService {
   constructor(private readonly llm: ILLM) {}
   // TODO: includes images
   public async execute(input: { prompt: string }): Promise<string> {
-    const i = new MessageInput(input.prompt);
+    const i = new TextInput(input.prompt);
     return (await this.llm.generate(i)).v;
   }
 }
