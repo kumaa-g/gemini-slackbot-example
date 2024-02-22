70d36f223b063b76b2622d9f389d585e0d7de33a
jupemara <jumpei.arashi@arashike.com>
2024-02-22 20:41:27 +0900

    slack app boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import { App } from '@slack/bolt';
+import { NullPoController } from './infra/controller/null-po/controller';
+import { NullPoService } from './service/null-po/service';
+
 (async () => {
-  console.log('just started');
+  // TODO: configure slack
+  const app = new App(),
+    controllers = {
+      nullpo: new NullPoController(new NullPoService()),
+    };
+  app.message('ぬるぽ', async ({ message, say }) => {
+    await controllers.nullpo.handle(message, say);
+  });
+  await app.start(process.env.PORT ?? 3000);
+  console.log('gemini slack example is running...');
 })();
