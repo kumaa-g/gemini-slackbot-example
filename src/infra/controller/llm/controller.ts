ace269642ad51029521c015b38d47491ff9c4597
jupemara <jumpei.arashi@arashike.com>
2024-02-27 22:40:48 +0900

    filedownload + upload to gemini

--- src/infra/controller/llm/controller.ts
+++ src/infra/controller/llm/controller.ts
@@ -1,19 +1,43 @@
 import { MessageEvent, SayFn } from '@slack/bolt';
 import { LLMService } from '~/service/llm/service';
+import { downloadSlackPrivateFile } from '~/util/slack/download';
 
 export class LLMController {
   constructor(private readonly service: LLMService) {}
   public async handle(message: MessageEvent, say: SayFn): Promise<void> {
-    let text = '';
+    let text = '',
+      images: string[] = [];
     if (message.subtype === undefined) {
-      text = (message.text ?? '').replace(/<\@.+>/, '');
+      text = this.stripMention(message.text ?? '');
     }
-    console.log('input:', text);
+    if (message.subtype === 'file_share' && !!message.files) {
+      try {
+        text = this.stripMention(message.text ?? '');
+        await Promise.all(
+          message.files.map(async (v) => {
+            if (!!v.url_private_download && !!v.name) {
+              images.push(
+                await downloadSlackPrivateFile(v.url_private_download, v.name),
+              );
+            }
+          }),
+        );
+      } catch (e) {
+        if (e instanceof Error) {
+          await say(e.message);
+        } else {
+          await say('予期せぬエラーが発生しました');
+        }
+      }
+    }
     await say(
       await this.service.execute({
         prompt: text,
-        images: [],
+        images,
       }),
     );
+  }
+  private stripMention(raw: string): string {
+    return raw.replace(/<\@.+>/, '');
   }
 }
