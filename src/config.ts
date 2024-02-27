dc75dfb022c405041c96e6016bfcaf1bb5c8be47
jupemara <jumpei.arashi@arashike.com>
2024-02-27 22:01:30 +0900

    configuration by environment variables

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,12 @@
 export const config = {
+  slack: {
+    signingSecret: process.env.SLACK_SIGNING_SECRET ?? '',
+    botToken: process.env.SLACK_OAUTH2_BOT_TOKEN ?? '',
+    socketMode: process.env.SLACK_SOCKET_MODE ?? 'false',
+    appToken: process.env.SLACK_APP_TOKEN ?? '',
+  },
   gcp: {
     project: process.env.GCP_PROJECT ?? '',
+    region: process.env.GCP_REGION ?? 'us-central1',
   },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import { App } from '@slack/bolt';
 import { NullPoController } from './infra/controller/null-po/controller';
 import { NullPoService } from './service/null-po/service';
+import { config } from './config';
+import { LLMController } from './infra/controller/llm/controller';
+import { LLMService } from './service/llm/service';
+import { Gemini } from './infra/repository/llm/gemini/repository';
+import { VertexAI } from '@google-cloud/vertexai';
 
 (async () => {
-  // TODO: configure slack
-  const app = new App(),
+  const app = new App({
+      signingSecret: config.slack.signingSecret,
+      token: config.slack.botToken,
+      socketMode: config.slack.socketMode.toLowerCase() === 'true',
+      appToken: config.slack.appToken,
+    }),
     controllers = {
+      gemini: new LLMController(
+        new LLMService(
+          new Gemini(
+            new VertexAI({
+              project: config.gcp.project,
+              location: config.gcp.region,
+            }),
+          ),
+        ),
+      ),
       nullpo: new NullPoController(new NullPoService()),
     };
   app.message('ぬるぽ', async ({ message, say }) => {
