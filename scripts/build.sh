044df2a9339ab662b3a584427cf0948f9029bf20
jupemara <jumpei.arashi@arashike.com>
2024-02-28 21:41:12 +0900

    just build docker image

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 CURRENT_SHA=$(git show --format='%H' --no-patch)
 IMAGE_URL="us-docker.pkg.dev/${GCP_PROJECT}/gemini-slack/gemini-slackbot-example:${CURRENT_SHA}"
+docker build -t ${IMAGE_URL}
 
-gcloud builds submit --tag $IMAGE_URL --project=${GCP_PROJECT}
 export IMAGE_URL
