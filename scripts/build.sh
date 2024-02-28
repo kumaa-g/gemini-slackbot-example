7f311da4156cbb88ecdb27ef725d10df7f864d58
jupemara <jumpei.arashi@arashike.com>
2024-02-28 21:52:33 +0900

    build context

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 CURRENT_SHA=$(git show --format='%H' --no-patch)
 IMAGE_URL="us-docker.pkg.dev/${GCP_PROJECT}/gemini-slack/gemini-slackbot-example:${CURRENT_SHA}"
-docker build -t ${IMAGE_URL}
+docker build ./ -t ${IMAGE_URL}
 
 export IMAGE_URL
