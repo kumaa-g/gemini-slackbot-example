75885bf6f611a280b285a8678fd2f9469efc8e71
jupemara <jumpei.arashi@arashike.com>
2024-02-28 21:31:55 +0900

    inject GCP_PROJECT

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 CURRENT_SHA=$(git show --format='%H' --no-patch)
-IMAGE_URL="us-docker.pkg.dev/$(gcloud config get-value project)/gemini-slack/gemini-slackbot-example:${CURRENT_SHA}"
+IMAGE_URL="us-docker.pkg.dev/${GCP_PROJECT}/gemini-slack/gemini-slackbot-example:${CURRENT_SHA}"
 
-gcloud builds submit --tag $IMAGE_URL
+gcloud builds submit --tag $IMAGE_URL --project=${GCP_PROJECT}
 export IMAGE_URL
