d9c70c6abc3866981666b9cbffeb15b9629f79a9
jupemara <jumpei.arashi@arashike.com>
2024-02-28 21:24:14 +0900

    export IMAGE_URL

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,5 @@
 CURRENT_SHA=$(git show --format='%H' --no-patch)
 IMAGE_URL="us-docker.pkg.dev/$(gcloud config get-value project)/gemini-slack/gemini-slackbot-example:${CURRENT_SHA}"
 
-gcloud builds submit --tag $DOCKER_IMAGE
+gcloud builds submit --tag $IMAGE_URL
+export IMAGE_URL
